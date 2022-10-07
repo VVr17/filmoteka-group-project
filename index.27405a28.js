001833758fbec196882eb732659659db70b91ce5
VVr17 <103305401+VVr17@users.noreply.github.com>
2022-10-07 20:48:30 +0000

    Deploying to gh-pages from @ VVr17/filmoteka-group-project@f0a070b3f5156687011d7e7da00b67275dcdd08d 🚀

--- index.27405a28.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#accordion").addEventListener("click",(function(t){if("BUTTON"===t.target.nodeName||t.target.closest("[data-accordion-btn]")){const e=t.target.closest("[data-accordion-btn]"),a="false"===e.getAttribute("aria-expanded")?"true":"false";e.setAttribute("aria-expanded",`${a}`),t.target.closest("li").classList.toggle("is-shown")}}));
-//# sourceMappingURL=index.27405a28.js.map
